e4c2c1d64c35d7a63b64c29bafdc24d9b7a56b02
nayo <skynet8252@gmail.com>
2025-02-18 12:49:20 -0500

    cambiecito

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-const miObjeto = {
-  "attr1" : 2,
-  "attr2" : "spring"
-}
-
-const objeto2 = miObjeto
-miObjeto.attr2 = "Nayo"
-console.log(miObjeto)
-console.log(objeto2)
-
-const objeto3 = {...miObjeto}
-objeto3.attr3 = "unNuevo atributo"
-console.log(objeto3)
-console.log(miObjeto)
-
-const objetoDeep = JSON.parse(JSON.stringify(miObjeto))
-
-
-const cuadradoForEach =[1,2,3].forEach(el => el*el);
-const cuadradoMap = [1,2,3].map(el => el*el);
-
-console.log(cuadradoMap)
-console.log(cuadradoForEach);
+console.log('ola mundo');
+console.log('ola mundo de zaza');
+console.log('ola mundo de zozo');
